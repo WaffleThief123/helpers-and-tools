5ae0dda204597be2db6bd9a45eb242830a6f1fc7
MissWaffleThief <mykeyboardisqwerty@gmail.com>
2020-12-01 01:19:06 -0800

    lets try that again..
    lets try that again..

--- new-vm-creation.sh
+++ new-vm-creation.sh
@@ -22,7 +22,10 @@ apt update && apt install rsync neofetch nmon vim curl htop wget build-essential
 sleep 1
 # Docker and Docker-Compose install
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
 apt update && apt-get install docker-ce docker-ce-cli containerd.io -y
 
 curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
